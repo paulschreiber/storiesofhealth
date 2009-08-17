d87594409d5fe12b7b07b1553a91f654e7c73b8e
paul <paul@2dbfc726-9c28-4bf9-9caf-3145de9bf07b>
2009-08-17 03:35:31 +0000

    added capistrano configuration

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "storiesofhealth"
+set :domain, "shrub.ca"
+set :use_sudo, false
+set :repository,  "http://#{domain}/paulsvn/storiesofhealth"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/data/sites/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+
+set :scm_user, "paul"
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
