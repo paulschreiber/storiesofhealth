bfeb4f62089a8851a881e10647d535813f16f1fe
paul <paul@2dbfc726-9c28-4bf9-9caf-3145de9bf07b>
2014-03-02 19:39:59 +0000

    Use Bundler

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+gem 'rails', '~> 2.3.18'
+gem 'mysql2', '~> 0.2.7'
+gem 'htmlentities'
+
+group :development do
+  gem 'capistrano', '~> 2', :require => false
+  gem 'capistrano-ext', :require => false
+end
--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "htmlentities"
+  # config.gem "htmlentities"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -34,12 +34,12 @@ Rails::Initializer.run do |config|
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = 'UTC'
+  config.time_zone = 'America/New York'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-  
+
   Encoding.default_external = Encoding::UTF_8
   Encoding.default_internal = Encoding::UTF_8
 end
