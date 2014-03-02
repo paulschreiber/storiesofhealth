c66ca9f1d0afbeed99baae205cfc4597e58f9635
paul <paul@2dbfc726-9c28-4bf9-9caf-3145de9bf07b>
2014-03-02 21:14:06 +0000

    use https links for youtube embedding

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,31 +1,31 @@
 class Story < ActiveRecord::Base
   has_and_belongs_to_many :tags
-  
+
   def image_id
     "#{first_name}-#{id}.jpg".downcase
   end
 
   def city_tag
     "/from/#{self.city.downcase.gsub(' ', '')}"
-  end  
-  
+  end
+
   def youtube_link
-    "http://youtube.com/watch?v=#{self.youtube_id}"
+    "https://youtube.com/watch?v=#{self.youtube_id}"
   end
 
   def youtube_embed_code
-    link = "http://www.youtube.com/v/#{self.youtube_id}"
+    link = "https://www.youtube.com/v/#{self.youtube_id}"
     if RAILS_ENV != "development"
       link += "&amp;autoplay=1"
     end
     link
   end
-  
+
   def tweet
     coder = HTMLEntities.new
-    coder.decode("#{self.description} #hcr #hc09 http://storiesofhealth.org/v/#{self.id}")
+    coder.decode("#{self.description} #hcr #hc09 https://storiesofhealth.org/v/#{self.id}")
   end
-  
+
   def tag_list
     self.tags.collect(&:name).join(" ")
   end
