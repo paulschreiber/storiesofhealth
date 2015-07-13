377a816e5ccf15cf62ebf8e7d0b0eacf9216db2f
Paul Schreiber <paulschreiber@gmail.com>
2015-07-13 00:27:00 -0400

    update capistrano files for Capistrano 3.4

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,37 +1,51 @@
-require 'bundler/capistrano'
+# config valid only for current version of Capistrano
+lock '3.4.0'
 
-set :application, "storiesofhealth"
-set :domain, "shrub.ca"
-set :use_sudo, false
-set :repository,  "https://#{domain}/paulsvn/storiesofhealth"
-set :deploy_via, :export
+set :application, 'storiesofhealth'
+set :repo_url, 'git@github.com:paulschreiber/storiesofhealth.git'
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/data/sites/#{application}"
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
 
-set :scm_user, "paul"
+# Default value for :scm is :git
+# set :scm, :git
 
-role :app, domain
-role :web, domain
-role :db,  domain, :primary => true
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, %w{config/database.yml config/secrets.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+set :passenger_restart_with_touch, true
 
 namespace :deploy do
-  task :start, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
   end
 
-  task :stop, :roles => :app do
-    # Do nothing.
-  end
-
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
-  end
 end
