d5e713139f3bb8a1108cfa6fc85cd8ce93b3f201
Paul Schreiber <paulschreiber@gmail.com>
2015-07-12 23:59:50 -0400

    update .find() to .where() [untested]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,18 @@
-class HomeController < ApplicationController  
+class HomeController < ApplicationController
   include HomeHelper
-  
+
   def index(story_count=2)
-    @stories = Story.find_by_sql("SELECT * FROM stories WHERE youtube_id IS NOT NULL ORDER BY RAND() LIMIT #{story_count}")
+    @stories = Story.where("youtube_id IS NOT NULL ORDER BY RAND() LIMIT #{story_count}")
     @story = @stories.first
     @selected_stories = @stories[0..story_count]
     @subtitle = "Every face has a story"
   end
-  
+
   def trailers
     @subtitle = "Trailers"
     @trailer_list = trailer_list
   end
-  
+
   def about
     @subtitle = "About"
   end
@@ -24,80 +24,80 @@ class HomeController < ApplicationController
   def share
     @subtitle = "Share Your Story"
   end
-  
+
   def stories
-    @stories = Story.find(:all, :conditions => "youtube_id IS NOT NULL", :order => "first_name")
+    @stories = Story.where("youtube_id IS NOT NULL").order("first_name")
     @subtitle = "Every face has a story"
     render :action => "list" and return
   end
-  
+
   def v
     redirect_to :action => :video
   end
-  
+
   def video
     if params[:id].nil?
       redirect_to :action => :stories
-    end 
+    end
 
-    @stories = Story.find(:all, :conditions => "youtube_id IS NOT NULL", :order => "first_name")
-    @story = Story.find(:first, :conditions => ["youtube_id IS NOT NULL AND id = ?", params[:id]])
+    @stories = Story.where("youtube_id IS NOT NULL").order("first_name")
+    @story = Story.where("youtube_id IS NOT NULL AND id = ?", params[:id]]).first
     @story = @stories.first if @story.nil?
     @subtitle = "#{@story.first_name}&rsquo;s story"
     @description = @story.description
-    
+
     @browser = true
   end
- 
+
   def from
-    @stories = Story.find(:all, :conditions => ["youtube_id IS NOT NULL AND (REPLACE(city, ' ', '') = ? OR city = ?)", params[:id], params[:id]], :order => "first_name")
+    @stories = Story.where("youtube_id IS NOT NULL AND (REPLACE(city, ' ', '') = ? OR city = ?)", params[:id], params[:id]]).order("first_name")
     @description = "Hear health care stories from people in #{@stories.first.city}, #{@stories.first.state}."
     return specified
   end
-  
+
   def tag
-    @tag = Tag.find(:all, :conditions => ["tags.name = ?", params[:id]])
+    @tag = Tag.where(name: params[:id])
     if @tag.empty?
       redirect_to :action => :index and return
     end
-    
-    @stories = Story.find(:all, :conditions => ["youtube_id IS NOT NULL AND tags.name = ?", params[:id]], :include => "tags", :order => "first_name")
+
+    @stories = Story.where("youtube_id IS NOT NULL AND tags.name = ?", params[:id]).include("tags").order("first_name")
     @description = "Hear health care stories from people who #{@tag.first.description_phrase}."
     return specified
   end
 
   def specified
-    @stories = Story.all if @stories.empty? 
+    @stories = Story.all if @stories.empty?
     @story = @stories.first
     @subtitle = "#{@story.first_name}&rsquo;s story"
-    
+
     @browser = true
     render :action => "video"
   end
-  
+
   # AJAX thumbnail loader
   def browser
     if params[:q] == "tag"
-      @stories = Story.find(:all, :conditions => ["youtube_id IS NOT NULL AND tags.name = ?", params[:id]], :include => "tags", :order => "first_name")
+      @stories = Story.where("youtube_id IS NOT NULL AND tags.name = ?", params[:id]).include("tags").order("first_name")
     elsif params[:q] == "from"
-      @stories = Story.find(:all, :conditions => ["youtube_id IS NOT NULL AND city = ?", params[:id]], :order => "first_name")
+      @stories = Story.where("youtube_id IS NOT NULL AND city = ?", params[:id]).order("first_name")
     end
 
-    @stories = Story.all if @stories.empty? 
+    @stories = Story.all if @stories.empty?
     render :partial => "browser"
   end
-  
+
   # AJAX story loader
   def fetch_story
     if params[:id].nil?
-      @story = Story.find(:first, :conditions => "youtube_id IS NOT NULL")
+      @story = Story.where("youtube_id IS NOT NULL").first
     else
-      @story = Story.find(:first, :conditions => ["youtube_id IS NOT NULL AND id = ?", params[:id]])
+      @story = Story.where("youtube_id IS NOT NULL AND id = ?", params[:id]).first
     end
     @story = Story.first if @story.nil?
     @subtitle = "#{@story.first_name}&rsquo;s story"
 
     @browser = false
     render :action => "video", :layout => false, :track => true
-  end  
+  end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,24 +1,24 @@
-module HomeHelper  
+module HomeHelper
   def permalink(story)
      url_for(:controller => :home, :action => :video, :id => story.id, :host => request.host, :port => request.port)
   end
-  
+
   def compute_tag_sentence(story)
     tag_list = []
-    
+
     if story.nil?
-      all_tags = Tag.find_by_sql("SELECT * FROM tags ORDER BY RAND() LIMIT 5")
+      all_tags = Tag.where("SELECT * FROM tags ORDER BY RAND() LIMIT 5")
       klass = nil
     else
       all_tags = story.tags
       klass = "tag"
     end
-    
-    all_tags.each do |t|      
-      tag_list << link_to(t.description_phrase, {:action => :tag, :id => t.name}, :class => klass) 
+
+    all_tags.each do |t|
+      tag_list << link_to(t.description_phrase, {:action => :tag, :id => t.name}, :class => klass)
     end
     @tag_sentence = tag_list.to_sentence(:last_word_connector => " and ")
-    
+
     if tag_list.size == 1
       @join_phrase = " and"
     else
@@ -29,7 +29,7 @@ module HomeHelper
   def trailer_list
     ["tePzuu1itkk", "K0Gdw7uv0Ec", "Tj_wqelhQ1g", "aCnMW_S5UFQ"]
   end
-  
+
   def random_trailer_id
     trailers = trailer_list
     return trailers[rand(trailers.size)]
