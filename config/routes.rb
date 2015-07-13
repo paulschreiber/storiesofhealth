d3364a3598991010107d412f28a5a41e901f0fa6
Paul Schreiber <paulschreiber@gmail.com>
2015-07-13 00:43:22 -0400

    update routes for rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Storiesofhealth::Application.routes.draw do
-  match '/' => 'home#index'
-  match 'about' => 'home#about', :as => :home
-  match 'share' => 'home#share', :as => :home
-  match 'action' => 'home#action', :as => :home
-  match 'stories' => 'home#stories', :as => :home
-  match 'trailers' => 'home#trailers', :as => :home
-  match 'video/:id' => 'home#video', :as => :home
-  match 'v/:id' => 'home#v', :as => :home
-  match 'from/:id' => 'home#from', :as => :home
-  match 'tag/:id' => 'home#tag', :as => :home
-  match 'browser/tag/:id' => 'home#browser', :as => :home, :q => 'tag'
-  match 'browser/from/:id' => 'home#browser', :as => :home, :q => 'from'
-  match '/:controller(/:action(/:id))'
+  get '/' => 'home#index'
+  get 'about' => 'home#about'
+  get 'share' => 'home#share'
+  get 'action' => 'home#action'
+  get 'stories' => 'home#stories'
+  get 'trailers' => 'home#trailers'
+  get 'video/:id' => 'home#video'
+  get 'v/:id' => 'home#v'
+  get 'from/:id' => 'home#from'
+  get 'tag/:id' => 'home#tag'
+  get 'browser/tag/:id' => 'home#browser', :q => 'tag'
+  get 'browser/from/:id' => 'home#browser', :q => 'from'
+  get '/:controller(/:action(/:id))'
 end
