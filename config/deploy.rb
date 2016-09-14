5022c5db7434c150646c853692d3eec01f6660b0
Paul Schreiber <paulschreiber@gmail.com>
2016-09-14 08:51:18 -0400

    bump capistrano to 3.6.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.6.1'
 
 set :application, 'storiesofhealth'
 set :repo_url, 'git@github.com:paulschreiber/storiesofhealth.git'
